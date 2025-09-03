d2ba7bd4437305b28b692c6005c81e64ec87635e
Glauber Guimarães Ferreira <glauber.guiferr.dev@gmail.com>
2025-09-03 08:23:44 -0300

    Estilização inicial
    Adição de estilos default ao header, main e footer.

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  standalone: false,
+  templateUrl: './new-moment.component.html',
+  styleUrl: './new-moment.component.css'
+})
+export class NewMomentComponent {
+
+}
